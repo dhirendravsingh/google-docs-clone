2aa7a9c481c15d30a35ea3841616cdf1f35fcca7
dhirendravsingh <dhirendravsingh09@gmail.com>
2025-02-01 05:19:10 +0530

    added the functionality of navbar tools

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -60,9 +60,9 @@ export const Ruler = ()=>{
      }
 
     return (
-        <div ref={rulerRef} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp} className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+        <div ref={rulerRef} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp} className="w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
             <div id="ruler-container"
-            className="max-w-[816px] mx-auto w-full h-full relative">
+            className="w-full h-full relative">
                 <Marker position={leftMargin} isLeft={true} isDragging={isDraggingLeft} onMouseDown={handleLeftMouseDown} onDoubleClick={handleLeftDoubleClick} />
                 <Marker position={rightMargin} isLeft={false} isDragging={isDraggingRight} onMouseDown={handleRightMouseDown} onDoubleClick={handleRighttDoubleClick} />
 
