0d1904623233a65b53918e7e2231a9574b7cfef7
dhirendravsingh <dhirendravsingh09@gmail.com>
2025-02-11 18:17:54 +0530

    implemented authentication using clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+//this file is created in order to store the configuration of the authentication module
+//this is to configure convex with clerk
+
+export default {
+    providers : [
+        {
+            domain : "https://assuring-garfish-42.clerk.accounts.dev",
+            applicationID : "convex"
+        }
+    ]
+}
