fe3e0a393679948c3c58919fe87271173a32d5e1
dhirendravsingh <dhirendravsingh09@gmail.com>
2025-03-20 16:36:43 +0530

    added the functionality to edit the title of the document

--- src/app/documents/[documentId]/document-input.tsx
+++ src/app/documents/[documentId]/document-input.tsx
@@ -1,5 +1,11 @@
+
 import {BsCloudCheck} from "react-icons/bs"
 import { Id } from "../../../../convex/_generated/dataModel"
+import { useRef, useState } from "react"
+import { useMutation } from "convex/react"
+import { api } from "../../../../convex/_generated/api"
+import { useDebounce } from "@/hooks/use-debounce"
+import { toast } from "sonner"
 
 interface DocumentProps { 
     title : string
@@ -7,11 +13,63 @@ interface DocumentProps {
 }
 
 export const DocumentInput = ({title ,id} : DocumentProps)=>{
+    const [value, setValue] = useState(title)
+    const [iseError, setIsError] = useState(false)
+    const [isPending, setIsPending] = useState(false)
+    const [isEditing, setIsEditing] = useState(false)
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const mutate = useMutation(api.documents.updateById)
+
+    const debouncedUpdate = useDebounce((newValue : string)=> {
+        if(newValue === title) return
+
+        setIsPending(true)
+        mutate({id , title : newValue})
+        .then(()=> toast.success("Document updated"))
+        .catch(()=> toast.error("Something went wrong"))
+        .finally(()=> setIsPending(false))
+    })
+
+    const onChange = (e : React.ChangeEvent<HTMLInputElement>)=> {
+        const newValue = e.target.value
+        setValue(newValue)
+        debouncedUpdate(newValue)
+    }
+
+    const handleSubmit = (e : React.FormEvent<HTMLFormElement>)=> {
+        e.preventDefault()
+
+        setIsPending(true)
+        mutate({id , title : value})
+        .then(()=> {toast.success("Document updated"),
+                    setIsEditing(false)
+        })
+        .catch(()=> toast.error("Something went wrong"))
+        .finally(()=> setIsPending(false))
+    }
+
+
     return (
         <div className="flex items-center gap-2">
-            <span className="text-lg px-1.5 cursor-pointer truncate">
+            {isEditing ? (
+                <form onSubmit={handleSubmit} className="relative w-fit max-w-[50ch]">
+                        <span className="invisible whitespace-pre px-1.5 text-lg">
+                            {value || " "}
+                        </span>    
+                        <input ref={inputRef} value={value} onBlur={()=> setIsEditing(false)} onChange={onChange} className="absolute inset-0 text-lg text-black px-1.5 bg-transparent truncate"/>
+                </form>
+            ) : (
+                <span 
+                onClick={()=> {
+                    setIsEditing(true)
+                    setTimeout(()=> {inputRef.current?.focus()}, 0)
+                }} className="text-lg px-1.5 cursor-pointer truncate">
                 {title}
             </span>
+            ) }
+            
             <BsCloudCheck/>
         </div>
     )
